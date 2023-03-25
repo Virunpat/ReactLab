fe186b678d27f84b9242afef0bc696dc8780ca81
Virunpat <119098874+Virunpat@users.noreply.github.com>
2023-03-26 00:32:55 +0700

    first commit

---
+++ src/components/CardList.jsx
@@ -0,0 +1,45 @@
+import { Box, Checkbox } from "@mui/material";
+
+
+function CardList({ todo, setState, state }) {
+  function handleCheck(e){
+    if (e.target.checked){
+      const result = state.filter((item) => item !== todo);
+      setState(result);
+    }
+  }
+  return (
+    <Box sx={wrapperStyle}>
+      <Checkbox onChange={(e) => handleCheck(e)} />
+      <div>{todo}</div>
+    </Box>
+    // <div
+    //   style={{
+    //     padding: "20px",
+    //     background: "#fefffe",
+    //     width: "500px",
+    //     borderRadius: "10px",
+    //     margin: "20px 0px",
+    //     fontSize: "25px",
+    //     color: "#7b7b7b",
+    //   }}
+    // >
+    //   This is Card
+    // </div>
+  );
+}
+const wrapperStyle = {
+  padding: "10px 20px",
+  background: "#fefffe",
+  width: { xs: "300px", md: "500px" },
+  borderRadius: "10px",
+  margin: "20px 0px",
+  fontSize: "25px",
+  color: "#7b7b7b",
+  display: "flex",
+  alignItem: "center",
+  justifyContent: "Space-between",
+  boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
+};
+
+export default CardList;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import './Profile.css';
+function Profile (){
+    return(
+        <div className="layout">
+            <img src=".\image\images (8).jfif" alt="Profile"/>
+            <h1 className="name">phing Virunpat</h1>
+            <hr></hr>
+        </div>
+    )
+}
+
+export default Profile;
